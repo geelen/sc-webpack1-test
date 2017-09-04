92877b6b19ac487e2125aa57bc0f79f258187e2b
Glen Maddern <glenmaddern@gmail.com>
2017-09-04 11:37:05 +1000

    working webpack1 example

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './config'
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
